ee661b69465be38b5d5828a36590af13f208e7ec
VidhyaShr <37873735+VidhyaShr@users.noreply.github.com>
2025-02-14 17:03:11 +0530

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -12,18 +12,7 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-import "cypress-cucumber-attach-screenshots-to-failed-steps"
-// Import commands.js using ES2015 syntax:
-import './Utils/generic'
-import './Utils/StockCommands'
-import './Utils/AdministratorCommands'
-import './Utils/BankCommands'
-import './Utils/authourizationcommands'
-import './Utils/comissioncommands'
-import './Utils/securityCommands'
-import './Utils/subscriberCommands'
-import './Utils/registration'
-//import 'cypress-failed-log'
+
 
 import '@shelex/cypress-allure-plugin';
 import '@cypress/code-coverage/support'
@@ -42,18 +31,3 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-//import "cypress-fail-fast";
-
-// const options = {
-//   // Log console output only
-//   collectTypes: ['cy:xhr', 'cy:request', 'cy:intercept'],
-//   xhr:
-//   {
-//     printRequestData: true
-//   }
-   
- 
-// };
-// //import 'cypress-failed-log'
-// require('cypress-terminal-report/src/installLogsCollector')(options);
